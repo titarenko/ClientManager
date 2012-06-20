67c69fda559ea941998e394ba02beba95978306f
Constantin Titarenko <constantin.titarenko@gmail.com>
2012-06-20 20:31:57 +0300

    Added documentation.

--- BinaryStudio.ClientManager.DomainModel.Tests/PollingEmailChecker.cs
+++ BinaryStudio.ClientManager.DomainModel.Tests/PollingEmailChecker.cs
@@ -2,15 +2,28 @@ using System;
 
 namespace BinaryStudio.ClientManager.DomainModel.Tests
 {
+    /// <summary>
+    /// Receives emails by periodic polling of server.
+    /// </summary>
     public class PollingEmailChecker
     {
         private readonly IEmailClient emailClient;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PollingEmailChecker"/> class.
+        /// </summary>
+        /// <param name="timer">The timer.</param>
+        /// <param name="emailClient">The email client.</param>
         public PollingEmailChecker(Timer timer, IEmailClient emailClient)
         {
             timer.OnTick += OnTick;
             this.emailClient = emailClient;
         }
+
+        /// <summary>
+        /// Occurs when new email is received from server.
+        /// </summary>
+        public event EventHandler<EmailReceivedEventArgs> EmailReceived;
 
         private void OnTick(object sender, EventArgs eventArgs)
         {
@@ -26,7 +39,5 @@ namespace BinaryStudio.ClientManager.DomainModel.Tests
                 }
             }
         }
-
-        public event EventHandler<EmailReceivedEventArgs> EmailReceived;
     }
 }
