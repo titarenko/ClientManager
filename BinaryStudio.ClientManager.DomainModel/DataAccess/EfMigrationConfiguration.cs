56ffdb5b1bd0091270ac84260806d58237d3f231
yakimechko <yakimechko@gmail.com>
2012-06-24 11:31:49 +0300

    issue #7 classes for data access are added

---
+++ BinaryStudio.ClientManager.DomainModel/DataAccess/EfMigrationConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.Entity.Migrations;
+
+namespace BinaryStudio.ClientManager.DomainModel.DataAccess
+{
+    public class EfMigrationConfiguration : DbMigrationsConfiguration<EfDataContext>
+    {
+        public EfMigrationConfiguration()
+        {
+            AutomaticMigrationsEnabled = true;
+#if DEBUG
+            AutomaticMigrationDataLossAllowed = true;
+#endif
+        }
+    }
+}
