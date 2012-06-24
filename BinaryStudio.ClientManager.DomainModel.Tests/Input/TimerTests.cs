6190f4c2a6f15f89e011346fc61a1add6d1f1868
Stanislav Diakovskyi <spiralwanttodie@gmail.com>
2012-06-24 04:03:56 +0300

    Refactored class Timer and its tests
    issue #4

---
+++ BinaryStudio.ClientManager.DomainModel.Tests/Input/TimerTests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Threading;
+using NUnit.Framework;
+
+namespace BinaryStudio.ClientManager.DomainModel.Tests.Input
+{
+    [TestFixture, Category("Integration")]
+    class TimerTests
+    {
+        private TimeSpan _timeSpan;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _timeSpan = new TimeSpan(0, 0, 0, 2);
+        }
+
+        [Test]
+        public void ShouldNot_RaiseOnTickEvent_WhenTimerIsDisabled()
+        {
+            // arrange
+            var isRaised = false;
+            var timer = new DomainModel.Infrastructure.Timer();
+            timer.OnTick += (sender, args) => isRaised = true;
+            timer.Interval = _timeSpan;
+
+            // act 
+            Thread.Sleep(_timeSpan);
+            Thread.Sleep(_timeSpan);
+            Thread.Sleep(_timeSpan);
+
+            // check
+            Assert.That(!isRaised);
+        }
+
+        [Test]
+        public void Should_RaiseOnTickEvent_WhenTimeIsGone()
+        {
+            // arrange
+            var isRaised = false;
+            var timer = new DomainModel.Infrastructure.Timer();
+            timer.OnTick += (sender, args) => isRaised = true;
+            timer.Interval = _timeSpan;
+
+            // act
+            timer.Enabled = true;
+            Thread.Sleep(_timeSpan);
+            Thread.Sleep(_timeSpan);
+            Thread.Sleep(_timeSpan);
+
+            // check
+            Assert.That(isRaised);
+        }
+    }
+}
--- BinaryStudio.ClientManager.DomainModel/Infrastructure/Timer.cs
+++ BinaryStudio.ClientManager.DomainModel/Infrastructure/Timer.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Threading;
 
 namespace BinaryStudio.ClientManager.DomainModel.Infrastructure
 {
@@ -7,26 +8,60 @@ namespace BinaryStudio.ClientManager.DomainModel.Infrastructure
     /// </summary>
     public class Timer
     {
-        /// <summary>
-        /// Defines how often OnTick event should be issued.
-        /// </summary>
-        public virtual TimeSpan Interval { get; set; }
-
-        /// <summary>
-        /// Enables or disables issuing OnTick events.
-        /// </summary>
-        public virtual bool Enabled { get; set; }
+        private readonly System.Threading.Timer _timer;
+        private TimeSpan _interval;
 
         /// <summary>
         /// Periodically occurs after specified time interval has elapsed.
         /// </summary>
         public event EventHandler OnTick;
 
+        /// <summary>
+        /// Enables or disables issuing OnTick events.
+        /// </summary>
+        public bool Enabled { get; set; }
+
+        public Timer()
+        {
+            _timer = new System.Threading.Timer(new TimerCallback(TimerProc));
+        }
+
+        /// <summary>
+        /// Defines how often OnTick event should be issued.
+        /// </summary>
+        public TimeSpan Interval
+        {
+            get
+            {
+                return _interval;
+            }
+            set
+            {
+                _interval = value;
+                _timer.Change(new TimeSpan(0), _interval);
+            }
+        }
+
+        /// <summary>
+        /// left for backward compatibility with FakeTimer
+        /// </summary>
         protected void RaiseOnTick(object sender, EventArgs e)
         {
             if (OnTick != null)
             {
                 OnTick(sender, e);
+            }
+        }
+
+        /// <summary>
+        /// Callback function which will be executed time to time.
+        /// </summary>
+        /// <param name="state">itself timer, not used yet</param>
+        private void TimerProc(object state)
+        {
+            if (Enabled)
+            {
+                RaiseOnTick(state, new EventArgs());
             }
         }
     }
