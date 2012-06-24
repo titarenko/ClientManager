6e084f2a47078cc2735e9fab74e66491066fb588
Ivan Zaporozhchenko <1van1111@mail.ru>
2012-06-24 10:46:50 +0300

    Class Person Created

---
+++ BinaryStudio.ClientManager.DomainModel/Entities/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BinaryStudio.ClientManager.DomainModel.Entities
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime CreationDate { get; set; }
+        public string Email { get; set; }
+    }
+}
