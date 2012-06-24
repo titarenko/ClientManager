bdd11d492c01a0c6328ba9f4621ce19b14a1e7ac
Ivan Zaporozhchenko <1van1111@mail.ru>
2012-06-24 10:51:55 +0300

    Comments to Person class was added closed #10

--- BinaryStudio.ClientManager.DomainModel/Entities/Person.cs
+++ BinaryStudio.ClientManager.DomainModel/Entities/Person.cs
@@ -5,12 +5,34 @@ using System.Text;
 
 namespace BinaryStudio.ClientManager.DomainModel.Entities
 {
+    /// <summary>
+    /// Class that represents model of person
+    /// </summary>
     public class Person
     {
+        /// <summary>
+        /// Id of person. It is a primary key
+        /// </summary>
         public int Id { get; set; }
+
+        /// <summary>
+        /// First name of person
+        /// </summary>
         public string FirstName { get; set; }
+
+        /// <summary>
+        /// Last name of person
+        /// </summary>
         public string LastName { get; set; }
+
+        /// <summary>
+        /// Date when person was added to system
+        /// </summary>
         public DateTime CreationDate { get; set; }
+
+        /// <summary>
+        /// Email adress of person
+        /// </summary>
         public string Email { get; set; }
     }
 }
