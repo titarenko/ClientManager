209c3f1518ce05f35baad8dd5b9ff35d573dccf8
Ivan Zaporozhchenko <1van1111@mail.ru>
2012-06-25 18:47:19 +0300

    Refactored MailMessageConverterTests and MailMessageConverter

--- BinaryStudio.ClientManager.DomainModel.Tests/Input/MailMessageConverterTests.cs
+++ BinaryStudio.ClientManager.DomainModel.Tests/Input/MailMessageConverterTests.cs
@@ -4,9 +4,11 @@ using System.Linq;
 using System.Net.Mail;
 using System.Text;
 using BinaryStudio.ClientManager.DomainModel;
+using BinaryStudio.ClientManager.DomainModel.DataAccess;
 using BinaryStudio.ClientManager.DomainModel.Entities;
 using BinaryStudio.ClientManager.DomainModel.Infrastructure;
 using BinaryStudio.ClientManager.DomainModel.Input;
+using Moq;
 using NUnit.Framework;
 
 namespace BinaryStudio.ClientManager.DomainModel.Tests.Input
@@ -18,7 +20,8 @@ namespace BinaryStudio.ClientManager.DomainModel.Tests.Input
         public void Should_ReturnMailMessageWithRightData_WhenCallingConvertMailMessageFromInputTypeToEntityType()
         {
             //arrange
-            var converter = new MailMessageConverter();
+            var mock = new Mock<IRepository>();
+            var converter = new MailMessageConverter(mock.Object);
             var receiver = new MailAddress("employee@gmail.com", "Employee Petrov");
             var mailMessage = new DomainModel.Input.MailMessage
                                   {
@@ -34,19 +37,19 @@ namespace BinaryStudio.ClientManager.DomainModel.Tests.Input
                                            LastName = "Petrov",
                                            Email = "employee@gmail.com"
                                        };
-            var personComparer = new PersonComparer();
-
             //act
             var result=converter.ConvertMailMessageFromInputTypeToEntityType(mailMessage);
 
             //assert
+            //some query. Don't know how to do it yet
+            //mock.Verify(x=>x.Query());
             Assert.AreEqual("This is Body", result.Body);
             Assert.AreEqual("This is Subject", result.Subject);
             Assert.AreEqual(new DateTime(2012, 1, 1), result.Date);
             Assert.AreEqual("Client", result.Sender.FirstName);
             Assert.AreEqual("Ivanov", result.Sender.LastName);
             Assert.AreEqual("client@gmail.com",result.Sender.Email);
-            Assert.That(result.Receivers.Contains(expectedReceiver,personComparer));
+            Assert.That(result.Receivers.Contains(expectedReceiver));
         }
     }
 }
--- BinaryStudio.ClientManager.DomainModel/Input/MailMessageConverter.cs
+++ BinaryStudio.ClientManager.DomainModel/Input/MailMessageConverter.cs
@@ -1,27 +1,35 @@
 using System.Collections.Generic;
+using BinaryStudio.ClientManager.DomainModel.DataAccess;
 using BinaryStudio.ClientManager.DomainModel.Entities;
 
 namespace BinaryStudio.ClientManager.DomainModel.Input
 {
     public class MailMessageConverter
     {
-        /// <summary>
-        /// This method converts Input.MailMessage type into Entities.MailMessage type
-        /// </summary>
-        /// <param name="message">Input.MailMessage type of mail message </param>
-        /// <param name="sender">Represents sender as Person class</param>
-        /// <param name="receivers">Represents reveivers as ICollection of Person class</param>
-        /// <returns>Entities.MailMessage type of mail message</returns>
-        public Entities.MailMessage ConvertMailMessageFromInputTypeToEntityType(MailMessage message, Person sender, ICollection<Person> receivers)
+        private IRepository repository;
+
+        public MailMessageConverter(IRepository repository)
         {
-            return new Entities.MailMessage
-                                    {
-                                        Sender = sender,
-                                        Date = message.Date,
-                                        Subject = message.Subject,
-                                        Receivers = receivers
-                                    };        
+            this.repository = repository;
         }
+
+        ///// <summary>
+        ///// This method converts Input.MailMessage type into Entities.MailMessage type
+        ///// </summary>
+        ///// <param name="message">Input.MailMessage type of mail message </param>
+        ///// <param name="sender">Represents sender as Person class</param>
+        ///// <param name="receivers">Represents reveivers as ICollection of Person class</param>
+        ///// <returns>Entities.MailMessage type of mail message</returns>
+        //public Entities.MailMessage ConvertMailMessageFromInputTypeToEntityType(MailMessage message, Person sender, ICollection<Person> receivers)
+        //{
+        //    return new Entities.MailMessage
+        //                            {
+        //                                Sender = sender,
+        //                                Date = message.Date,
+        //                                Subject = message.Subject,
+        //                                Receivers = receivers
+        //                            };        
+        //}
 
         public Entities.MailMessage ConvertMailMessageFromInputTypeToEntityType(MailMessage mailMessage)
         {
