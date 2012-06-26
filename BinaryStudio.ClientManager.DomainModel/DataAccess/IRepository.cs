7803db52c2bcc2ebc03ea72483bf107b3a533f34
Constantin Titarenko <constantin.titarenko@gmail.com>
2012-06-26 22:27:05 +0300

    Added documentation for Query method.

--- BinaryStudio.ClientManager.DomainModel/DataAccess/IRepository.cs
+++ BinaryStudio.ClientManager.DomainModel/DataAccess/IRepository.cs
@@ -7,6 +7,15 @@ namespace BinaryStudio.ClientManager.DomainModel.DataAccess
 {
     public interface IRepository
     {
+        /// <summary>
+        /// Returns query interface the specified entity.
+        /// </summary>
+        /// <typeparam name="T">Entity type.</typeparam>
+        /// <param name="eagerlyLoadedProperties">The eagerly loaded properties - not query itself!</param>
+        /// <returns>Interface for querying.</returns>
+        /// <example>
+        /// repository.Query&lt;MyEntity&gt;().Where(x => x.Name == "Entity1")
+        /// </example>
         IQueryable<T> Query<T>(params Expression<Func<T, object>>[] eagerlyLoadedProperties) where T : class, IIdentifiable;
         T Get<T>(int id, params Expression<Func<T, object>>[] eagerlyLoadedProperties) where T : class, IIdentifiable;
 
