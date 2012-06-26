633631a5561b5cacddbc99a7d2e963f93c5c52f5
yakimechko <yakimechko@gmail.com>
2012-06-26 09:55:18 +0300

    issue #17 added View for displaying full list of clients.

--- BinaryStudio.ClientManager.WebUi.Tests/Controllers/ClientsControllerTests.cs
+++ BinaryStudio.ClientManager.WebUi.Tests/Controllers/ClientsControllerTests.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Web.Mvc;
 using BinaryStudio.ClientManager.WebUi.Controllers;
 using BinaryStudio.ClientManager.DomainModel.Entities;
 using BinaryStudio.ClientManager.DomainModel.DataAccess;
--- BinaryStudio.ClientManager.WebUi/Controllers/ClientsController.cs
+++ BinaryStudio.ClientManager.WebUi/Controllers/ClientsController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using BinaryStudio.ClientManager.DomainModel.DataAccess;
 using BinaryStudio.ClientManager.DomainModel.Entities;
 
