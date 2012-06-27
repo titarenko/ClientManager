e4291bead416c5069948e68cf171c3380fe166ab
Constantin Titarenko <constantin.titarenko@gmail.com>
2012-06-27 12:15:38 +0300

    Added documentation.

--- BinaryStudio.ClientManager.DomainModel/Infrastructure/IIdentifiable.cs
+++ BinaryStudio.ClientManager.DomainModel/Infrastructure/IIdentifiable.cs
@@ -1,7 +1,13 @@
 namespace BinaryStudio.ClientManager.DomainModel.Infrastructure
 {
+    /// <summary>
+    /// Used for marking entities which have unique identifier.
+    /// </summary>
     public interface IIdentifiable
     {
+        /// <summary>
+        /// Unique identifier.
+        /// </summary>
         int Id { get; }
     }
 }
