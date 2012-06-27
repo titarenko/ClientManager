8c24a9e19cf5e555a36eb12e3855964e17481514
Kirill <tztimezero@yandex.ru>
2012-06-27 23:12:53 +0300

    Issue #16
    Added Dashboard model, controller and view.

---
+++ BinaryStudio.ClientManager.WebUi/Controllers/DashboardController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BinaryStudio.ClientManager.DomainModel.DataAccess;
+using BinaryStudio.ClientManager.DomainModel.Entities;
+using BinaryStudio.ClientManager.WebUi.Models;
+
+namespace BinaryStudio.ClientManager.WebUi.Controllers
+{
+    public class DashboardController : Controller
+    {
+        //
+        // GET: /Dashboard/
+
+        private readonly IRepository repository;
+
+        public DashboardController(IRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        public ActionResult Index()
+        {
+            DashboardModel model = new DashboardModel(); 
+            model.inquiries = repository.Query<Inquiry>();
+            model.employees = repository.Query<Person>().Where(person => person.Role == PersonRole.Employee);
+            
+            return View(model);
+        }
+
+    }
+}
---
+++ BinaryStudio.ClientManager.WebUi/Models/DashboardModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Linq;
+using BinaryStudio.ClientManager.DomainModel.Entities;
+
+namespace BinaryStudio.ClientManager.WebUi.Models
+{
+    public class DashboardModel
+    {
+        public IQueryable<Inquiry> inquiries { get; set; }
+        public IQueryable<Person> employees { get; set; }
+    }
+}
