d5f8dcedb005fccc9c16e9be78d84edefbb702c7
Stanislav Diakovskyi <spiralwanttodie@gmail.com>
2012-07-03 01:35:21 +0300

    added references to the new modules, added helper class RandomItem (#25)
    close #25

---
+++ BinaryStudio.ClientManager.WebUi.Tests/Infrastructure/RandomItem.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using FizzWare.NBuilder;
+
+namespace BinaryStudio.ClientManager.WebUi.Tests.Infrastructure
+{
+    class RandomItem<T>
+    {
+        private readonly IList<T> list;
+        private readonly IRandomGenerator randomGenerator;
+
+        /// <summary>
+        /// constructor
+        /// </summary>
+        /// <param name="list"> our collection</param>
+        /// <param name="unique">if true then random item will be unique; if false then possible recurrence of items</param>
+        public RandomItem(IList<T> list, bool unique = true)
+        {
+            this.list = list;
+            randomGenerator = unique ? new UniqueRandomGenerator() : new RandomGenerator();
+        }
+
+        /// <summary>
+        /// Returns next random item
+        /// </summary>
+        /// <returns></returns>
+        public T Next()
+        {
+            return list[randomGenerator.Next(0, list.Count - 1)];
+        }
+    }
+}
