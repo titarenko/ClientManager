d1c0f5c73634a5a2b5eccfe946afd789d497724c
Kirill Buga <kirill.buga@binary-studio.com>
2012-07-02 16:29:16 +0300

    Added ExtensionTests

---
+++ BinaryStudio.ClientManager.DomainModel.Tests/Input/ExtensionsTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq.Expressions;
+using BinaryStudio.ClientManager.DomainModel.Entities;
+using NUnit.Framework;
+
+namespace BinaryStudio.ClientManager.DomainModel.Tests.Input
+{
+    [TestFixture]
+    class ExtensionsTests
+    {
+        [Test]
+        public void Shoud_ReturnSourceDotSender_WhenXDotSourceDotSenderExpressionIsPassed()
+        {
+            Expression<Func<Inquiry, Person>> expression = x => x.Source.Sender;
+            var e = expression.GetPath();
+            Assert.AreEqual("Source.Sender", e);
+        }
+
+        [Test]
+        public void Shoud_ReturnSource_WhenXDotSourceExpressionIsPassed()
+        {
+            Expression<Func<Inquiry, MailMessage>> expression = x => x.Source;
+            var e = expression.GetPath();
+            Assert.AreEqual("Source", e);
+        }
+
+        [Test]
+        public void Shoud_ReturnRelatedMailsDotSender_WhenXDotRelatedMails0DotSender()
+        {
+            Expression<Func<Person, Person>> expression = x => x.RelatedMails[0].Sender;
+            var e = expression.GetPath();
+            Assert.AreEqual("RelatedMails.Sender", e);
+        }
+    }
+}
