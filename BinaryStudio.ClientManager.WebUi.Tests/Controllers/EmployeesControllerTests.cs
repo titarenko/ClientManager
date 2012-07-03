790ffa02a90568f2d776a6ba5f1e5cbd6d42890f
Ivan Zaporozhchenko <1van1111@mail.ru>
2012-07-03 22:27:11 +0300

    All tests of EmployeesControllerTests.cs was fixed in implementation. issue #33

---
+++ BinaryStudio.ClientManager.WebUi.Tests/Controllers/EmployeesControllerTests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BinaryStudio.ClientManager.DomainModel.DataAccess;
+using BinaryStudio.ClientManager.DomainModel.Entities;
+using BinaryStudio.ClientManager.WebUi.Controllers;
+using FizzWare.NBuilder;
+using FluentAssertions;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace BinaryStudio.ClientManager.WebUi.Tests.Controllers
+{
+    [TestFixture]
+    public class EmployeesControllerTests
+    {
+        [Test]
+        public void Should_ReturnListOf200Employees_WhenRequestedIndexWith200EmployeesExistsInRepository()
+        {
+            //arrange
+            var data = Builder<Person>.CreateListOfSize(500)
+                .All()
+                .With(x => x.Role = PersonRole.Client)               
+                .Random(200)
+                .With(x => x.Role = PersonRole.Employee)
+                .Build()
+                .AsQueryable();
+
+            var repository = Substitute.For<IRepository>();
+            repository.Query<Person>().Returns(data);
+
+            var employeesController = new EmployeesController(repository);
+
+            //act
+            var model = (IEnumerable<Person>)employeesController.Index().Model;
+
+            //assert
+            model.Count().Should().Be(200);
+        }
+
+        [Test]
+        public void ShouldNot_RiseException_WhenListOfEmployeeIsEmpty()
+        {
+            //arrange
+            var repository = Substitute.For<IRepository>();
+            repository.Query<Person>().Returns(new List<Person>().AsQueryable());
+            
+            var employeesController = new EmployeesController(repository);
+
+            //act
+            employeesController.Index();
+
+            //assert
+            Assert.Pass();
+        }
+
+    }
+}
---
+++ BinaryStudio.ClientManager.WebUi/Controllers/EmployeesController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BinaryStudio.ClientManager.DomainModel.DataAccess;
+using BinaryStudio.ClientManager.DomainModel.Entities;
+
+namespace BinaryStudio.ClientManager.WebUi.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        private IRepository repository;
+
+        public EmployeesController(IRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        //
+        // GET: /Employees/
+        public ViewResult Index()
+        {
+            return View(repository.Query<Person>().ToList().Where(x=>x.Role==PersonRole.Employee));
+        }
+
+    }
+}
