b6bb978b6b2706e1b7815cbc9621f848fe07b727
Alex Klyuchnik <alex.aquasky2@gmail.com>
2012-07-03 17:13:17 +0300

    Removing of garbage
    Remove some garbage left from previous commit

--- BinaryStudio.ClientManager.WebUi.Tests/Controllers/ClientsControllerTests.cs
+++ BinaryStudio.ClientManager.WebUi.Tests/Controllers/ClientsControllerTests.cs
@@ -21,37 +21,6 @@ namespace BinaryStudio.ClientManager.WebUi.Tests.Controllers
         [SetUp]
         public void CreateClientsList()
         {
-            
-            /*  clients = new[]
-                {
-                    new Person
-                        {
-                            CreationDate = new DateTime(2010, 1, 27),
-                            Email = "client1@mail.ru",
-                            FirstName = "Peter",
-                            LastName = "Petrov",
-                            RoleValue = (int)PersonRole.Client,
-                            Id = 3
-                        },
-                    new Person
-                        {
-                            CreationDate = new DateTime(2011, 9, 17),
-                            Email = "client2@gmail.com",
-                            FirstName = "Sidor",
-                            LastName = "Sidorov",
-                            RoleValue = (int)PersonRole.Employee,
-                            Id = 7
-                        },
-                    new Person
-                        {
-                            CreationDate = new DateTime(2012, 6, 16),
-                            Email = "client3@gmail.com",
-                            FirstName = "Ivan",
-                            LastName = "Ivanov",
-                            RoleValue = (int)PersonRole.Client,
-                            Id = 13
-                        }
-                */
         }
 
         private readonly MailMessage[] messages = {
@@ -111,16 +80,6 @@ namespace BinaryStudio.ClientManager.WebUi.Tests.Controllers
         [Test]
         public void Should_Return300_WhenFillsListWith300Employees()
         {
-            /*var mock = new Mock<IRepository>();
-            mock.Setup(x => x.Query<Person>()).Returns(clients.AsQueryable());
-            var clientController = new ClientsController(mock.Object);
-
-            var viewResult = clientController.Index();
-            var viewResultModel = viewResult.Model as IEnumerable<Person>;
-
-            Assert.IsNotNull(viewResultModel.SingleOrDefault(x => x.Id == 3));
-            Assert.IsNull(viewResultModel.SingleOrDefault(x => x.Id == 7));
-            Assert.IsNotNull(viewResultModel.SingleOrDefault(x => x.Id == 13));*/
             
             //arrange
             var data = Builder<Person>.CreateListOfSize(1000)
@@ -128,7 +87,6 @@ namespace BinaryStudio.ClientManager.WebUi.Tests.Controllers
                 .With(x => x.Role = PersonRole.Employee)
                 .Random(300)
                 .With(x => x.Role = PersonRole.Client)
-                .Random(700)
                 .Build().AsQueryable();
 
             var repository = Substitute.For<IRepository>();
