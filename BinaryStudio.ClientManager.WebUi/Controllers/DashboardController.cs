a368faebed92f1d3e8881c55f88126bfd433a4ab
Stanislav Diakovskyi <spiralwanttodie@gmail.com>
2012-07-04 06:28:35 +0300

    refixed error in DashBoardController

--- BinaryStudio.ClientManager.WebUi/Controllers/DashboardController.cs
+++ BinaryStudio.ClientManager.WebUi/Controllers/DashboardController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Web.Mvc;
 using BinaryStudio.ClientManager.DomainModel.DataAccess;
@@ -22,16 +23,20 @@ namespace BinaryStudio.ClientManager.WebUi.Controllers
         public ActionResult Index()
         {
             var model = new DashboardModel();
+            var employeesModelList = new List<SelectListItem>();
+            var employeesFullList = repository.Query<Person>().Where(person => person.Role == PersonRole.Employee);
 
-            model.Employees = new SelectList(
-                repository.Query<Person>()
-                    .Where(person => person.Role == PersonRole.Employee)
-                    .Select(employee => new SelectListItem
-                                            {
-                                                Value = Convert.ToString(employee.Id),
-                                                Text = employee.FullName
-                                            })
-                    .ToList());
+            foreach (var employee in employeesFullList)
+            {
+                var id = employee.Id.ToString();
+                employeesModelList.Add(new SelectListItem
+                                           {
+                                               Value = id,
+                                               Text = employee.FullName
+                                           });
+            }
+
+            model.Employees = new SelectList(employeesModelList);
 
 
             var allInquiries = repository.Query<Inquiry>(x => x.Client, x => x.Source);
