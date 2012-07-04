7583469e5fc78f441d99b2aa734782fd1058ff4e
Alex Klyuchnik <alex.aquasky2@gmail.com>
2012-07-04 23:52:56 +0300

    Closed issue #35
    Implemented Event-Based email client

---
+++ BinaryStudio.ClientManager.DomainModel.Tests/Input/AeEventBasedEmailClientTest.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace BinaryStudio.ClientManager.DomainModel.Tests.Input
+{
+    [TestFixture]
+
+    class AeEventBasedEmailClientTest
+    {
+        [Test]
+
+        public void Should_RecieveMessagesFromServer_WhenThereIsAnyMessages()
+        {
+            Assert.AreEqual(1,1); //Test should be here
+        }
+
+    }
+}
---
+++ BinaryStudio.ClientManager.DomainModel/Input/AeEventBasedEmailClient.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using AE.Net.Mail;
+using BinaryStudio.ClientManager.DomainModel.Infrastructure;
+
+namespace BinaryStudio.ClientManager.DomainModel.Input
+{
+    public class AeEventBasedEmailClient : IEmailClient
+    {
+        private List<MailMessage>  unread = new List<MailMessage>();
+
+        private ImapClient client;
+
+        public AeEventBasedEmailClient(IConfiguration configuration)
+        {
+            configuration = configuration.GetSubsection("EmailClient");
+            client = new ImapClient(
+                configuration.GetValue("Host"),
+                configuration.GetValue("Username"),
+                configuration.GetValue("Password"),
+                ImapClient.AuthMethods.Login,
+                configuration.GetValue<int>("Port"),
+                configuration.GetValue<bool>("Secure"),
+                configuration.GetValue<bool>("SkipSslValidation"));
+            client.NewMessage += (sender, args) =>
+                                     {
+                                         if (args.MessageCount > 0)
+                                         {
+                                             unread.AddRange(client.SearchMessages(SearchCondition.New())
+                                                                 .Select(message => new MailMessage
+                                                                                        {
+                                                                                            Date = message.Value.Date,
+                                                                                            Sender =
+                                                                                                message.Value.Sender,
+                                                                                            Receivers = message.Value.To,
+                                                                                            Subject =
+                                                                                                message.Value.Subject,
+                                                                                            Body = message.Value.Body
+                                                                                        }));
+                                         }
+                                     };
+        }
+
+        public IEnumerable<MailMessage> GetUnreadMessages() //renew count of unread messages
+        {
+            var temp = unread;
+            unread = new List<MailMessage>();
+            return temp;
+        }
+    }
+}
