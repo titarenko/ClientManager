dfb07adaba3e67b93131dd546690831798bf4387
Stanislav Diakovskyi <spiralwanttodie@gmail.com>
2012-07-10 12:57:51 +0300

    added safe extensions

---
+++ BinaryStudio.ClientManager.DomainModel/Infrastructure/SafeExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace BinaryStudio.ClientManager.DomainModel.Infrastructure
+{
+    /// <summary>
+    /// implements extension metods for safe chained calls
+    /// for more information see http://devtalk.net/csharp/chained-null-checks-and-the-maybe-monad/
+    /// </summary>
+    public static class SafeExtensions
+    {
+        public static TResult SafeWith<TInput, TResult>(this TInput inputObject, Func<TInput, TResult> evaluator)
+            where TResult : class where TInput : class
+        {
+            return (null == inputObject) ? null : evaluator(inputObject);
+        }
+
+        public static TResult SafeGet<TInput, TResult>(this TInput inputObject, Func<TInput, TResult> evaluator, TResult defaultValue)
+            where TInput : class
+        {
+            return (null == inputObject) ? defaultValue : evaluator(inputObject);
+        }
+    }
+}
