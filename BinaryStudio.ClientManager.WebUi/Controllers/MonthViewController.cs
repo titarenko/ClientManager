900f6d0ba2647e0681a3c51d85ac14329a26fcec
Alex Klyuchnik <alex.aquasky2@gmail.com>
2012-07-12 23:19:13 +0300

    Begining of MonthView implementation #55
    Created basis of Controller, Model and View of Month Model

---
+++ BinaryStudio.ClientManager.WebUi/Controllers/MonthViewController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BinaryStudio.ClientManager.DomainModel.DataAccess;
+using BinaryStudio.ClientManager.WebUi.Models;
+using BinaryStudio.ClientManager.DomainModel.Entities;
+
+namespace BinaryStudio.ClientManager.WebUi.Controllers
+{
+    public class MonthViewController : Controller
+    {
+        private readonly IRepository repository;
+
+        public MonthViewController(IRepository repository)
+        {
+            this.repository = repository;
+        }
+        //
+        // GET: /MonthView/
+
+        public ActionResult Index()
+        {
+            ViewBag.currentDay = DateTime.Today;
+            var model = new MonthViewModel();
+            var inquiryTodayList = repository.Query<Inquiry>().
+                Where(inquiry => inquiry.ReferenceDate == DateTime.Today).ToList();
+
+            model.Inquiries = inquiryTodayList; // Fill inquiries with today inquiries
+
+            return View();
+        }
+
+    }
+}
---
+++ BinaryStudio.ClientManager.WebUi/Models/MonthViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using BinaryStudio.ClientManager.DomainModel.Entities;
+
+namespace BinaryStudio.ClientManager.WebUi.Models
+{
+    public class MonthViewModel
+    {
+        public IList<Inquiry> Inquiries { get; set; }
+        public List<Person> Clients { get; set; }
+    }
+}
