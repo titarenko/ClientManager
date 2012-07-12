b90d2f97ac482394735fcd6988c6bd848484deb1
Stanislav Diakovskyi <spiralwanttodie@gmail.com>
2012-07-12 11:27:20 +0300

    close #51
    added tag entity class

---
+++ BinaryStudio.ClientManager.DomainModel/Entities/TagEntity.cs
@@ -0,0 +1,12 @@
+﻿namespace BinaryStudio.ClientManager.DomainModel.Entities
+{
+    /// <summary>
+    /// adds tagging to inquiry
+    /// </summary>
+    public class TagEntity
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
