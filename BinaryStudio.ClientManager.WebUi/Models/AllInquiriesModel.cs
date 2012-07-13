666d54a4e7f40b8dcb45986166725e10d3cf3509
Stanislav Diakovskyi <spiralwanttodie@gmail.com>
2012-07-13 22:59:09 +0300

    added controller for #56

---
+++ BinaryStudio.ClientManager.WebUi/Models/AllInquiriesModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using BinaryStudio.ClientManager.DomainModel.Entities;
+
+namespace BinaryStudio.ClientManager.WebUi.Models
+{
+    public class AllInquiriesModel
+    {
+        public IList<Inquiry> Inquiries { get; set; }
+
+        public IList<TagEntity> Tags { get; set; }
+    }
+}
