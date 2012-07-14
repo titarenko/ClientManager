544a1516926eaa025761851384921802037a7e75
Constantin Titarenko <constantin.titarenko@gmail.com>
2012-07-14 15:40:08 +0300

    Fixed photo display (WeekView/Index).

---
+++ BinaryStudio.ClientManager.DomainModel/Infrastructure/StringExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace BinaryStudio.ClientManager.DomainModel.Infrastructure
+{
+    public static class StringExtensions
+    {
+        public static bool IsNullOrEmpty(this string line)
+        {
+            return String.IsNullOrWhiteSpace(line);
+        }
+
+        public static string Or(this string line, string defaultValue)
+        {
+            return line.IsNullOrEmpty() ? defaultValue : line;
+        }
+    }
+}
