c0cb9a76b797479b7cdae23ac866d6ff45c1507b
Alex Klyuchnik <alex.aquasky2@gmail.com>
2012-07-16 17:03:51 +0300

    Revert "Merged MonthViewController into Inquiries"
    This reverts commit ff2e064085cf5578bb51ec1506b7ca619279ba34.

--- BinaryStudio.ClientManager.WebUi/Controllers/InquiriesController.cs
+++ BinaryStudio.ClientManager.WebUi/Controllers/InquiriesController.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Web.Mvc;
 using BinaryStudio.ClientManager.DomainModel.DataAccess;
 using BinaryStudio.ClientManager.DomainModel.Entities;
-using BinaryStudio.ClientManager.WebUi.Models;
 
 namespace BinaryStudio.ClientManager.WebUi.Controllers
 {
@@ -59,41 +58,6 @@ namespace BinaryStudio.ClientManager.WebUi.Controllers
             return View(repository.Get<MailMessage>(id, x => x.Sender, x => x.Receivers));
         }
 
-        public IList<Inquiry> SelectedDayInquiries(DateTime day, IRepository repository)
-        {
-
-            return repository.Query<Inquiry>().Where(inquiry => inquiry.ReferenceDate == day).ToList();
-        }
-        //
-        // GET: /MonthView/
-
-        public ActionResult MonthView()
-        {
-            ViewBag.currentDay = DateTime.Today;
-            var model = new MonthViewModel();
-            var inquiryFutureList = repository.Query<Inquiry>().
-                Where(inquiry => inquiry.ReferenceDate >= DateTime.Today).ToList();
-
-            model.Inquiries = inquiryFutureList;
-
-            var MonthList = SelectedDayInquiries(DateTime.Today, repository);
-            for (int i = 1; i <= 31; i++)
-            {
-                var day = DateTime.Today.AddDays(i);
-                var dayInquiryList = SelectedDayInquiries(day, repository);
-                MonthList.Concat(dayInquiryList);
-            }
-
-            model.MonthViewItems = MonthList;
-
-            //var monthviewItems = new MonthViewItem();
-            //inquiryFutureList.ForEach(monthviewItems.ReferenceDate = inquiryFutureList[this].ReferenceDate, 
-            //    monthviewItems.Client = inquiryFutureList[this].Client);
-
-            //model.MonthViewItems = monthviewItems;
-
-            return View(model);
-        }
 
     }
 }
