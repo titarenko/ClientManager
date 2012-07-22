1230e1d0e5175c25ad4261295e96335138fe8dbc
Ivan Zaporozhchenko <1van1111@mail.ru>
2012-07-22 11:34:34 +0300

    Added CssClass to Tag entity

--- BinaryStudio.ClientManager.DomainModel/Entities/Tag.cs
+++ BinaryStudio.ClientManager.DomainModel/Entities/Tag.cs
@@ -10,5 +10,7 @@ namespace BinaryStudio.ClientManager.DomainModel.Entities
         public int Id { get; set; }
 
         public string Name { get; set; }
+
+        public string CssClass { get; set; }
     }
 }
