8f81d76c484c8727bd613904602ad493dc52fcb5
Alex Klyuchnik <alex.aquasky2@gmail.com>
2012-07-22 19:15:30 +0300

    Issue #67 implemented

--- BinaryStudio.ClientManager.WebUi/Models/MonthViewModel.cs
+++ BinaryStudio.ClientManager.WebUi/Models/MonthViewModel.cs
@@ -4,6 +4,8 @@ namespace BinaryStudio.ClientManager.WebUi.Models
 {
     public class MonthViewModel
     {
+        public int SkippedDays { get; set; }
+
         public IEnumerable<MonthItemViewModel> Days { get; set; }
     }
 }
