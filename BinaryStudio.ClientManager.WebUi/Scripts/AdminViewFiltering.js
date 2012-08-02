5fc60cf30367fc2db17a3f5d7275d0db9e7160ac
Ivan Zaporozhchenko <1van1111@mail.ru>
2012-08-02 17:14:22 +0300

    Added scripts

---
+++ BinaryStudio.ClientManager.WebUi/Scripts/AdminViewFiltering.js
@@ -0,0 +1,49 @@
+﻿$(function() {
+    window.viewModel.Filter = ko.observable();
+        
+    window.viewModel.ClickAssign = function(currentInquiry, employee) {
+        OnClickAssign(employee.Id, currentInquiry.Id);
+    };
+        
+    window.viewModel.ClickAddTag = function(currentInquiry, tag) {
+        OnClickAddTag(tag.Id, currentInquiry.Id);
+    };
+        
+    window.viewModel.ClickMoveTo = function(date, currentInquiry) {
+        OnClickMoveTo(currentInquiry.Id,date);
+    };
+        
+    window.viewModel.ClickAddComment = function(currentInquiry) {
+        ShowModal(currentInquiry.Id);
+    };
+        
+    for (var inquiryIndex in window.viewModel.Inquiries) {
+        var inquiry = window.viewModel.Inquiries[inquiryIndex];
+        inquiry.SkypeLink = ko.computed(function() {
+            return 'skype:' + inquiry.Phone + '?call';
+        }, window.viewModel);
+                
+        inquiry.EmailLink = ko.computed(function() {
+            return 'mailto:' + inquiry.Email;
+        }, window.viewModel);
+                
+        inquiry.Visible = ko.computed(function() {
+            if (!this.Filter() || this.Filter().length == 0) {
+                return true;
+            }
+            var localInquiry = this.Inquiry;
+            for (var property in localInquiry) {
+                if (localInquiry.hasOwnProperty(property) &&
+                    String(localInquiry[property]).contains(this.Filter())) {
+                    return true;
+                }
+            }
+            return false;
+        }, {
+            Filter: window.viewModel.Filter,
+            Inquiry: inquiry
+        });
+    }
+
+    ko.applyBindings(window.viewModel);
+});
---
+++ BinaryStudio.ClientManager.WebUi/Scripts/WeekViewFiltering.js
@@ -0,0 +1,52 @@
+﻿$(function() {
+    window.viewModel.Filter = ko.observable();
+        
+    window.viewModel.ClickAssign = function(currentInquiry, employee) {
+        OnClickAssign(employee.Id, currentInquiry.Id);
+    };
+        
+    window.viewModel.ClickAddTag = function(currentInquiry, tag) {
+        OnClickAddTag(tag.Id, currentInquiry.Id);
+    };
+        
+    window.viewModel.ClickMoveTo = function(date, currentInquiry) {
+        OnClickMoveTo(currentInquiry.Id,date);
+    };
+        
+    window.viewModel.ClickAddComment = function(currentInquiry) {
+        ShowModal(currentInquiry.Id);
+    };
+
+    for (var dayIndex in window.viewModel.Days) {
+        var day = window.viewModel.Days[dayIndex];
+        for (var inquiryIndex in day.Inquiries) {
+            var inquiry = day.Inquiries[inquiryIndex];
+            inquiry.SkypeLink = ko.computed(function() {
+                return 'skype:' + inquiry.Phone + '?call';
+            }, window.viewModel);
+                
+            inquiry.EmailLink = ko.computed(function() {
+                return 'mailto:' + inquiry.Email;
+            }, window.viewModel);
+                
+            inquiry.Visible = ko.computed(function() {
+                if (!this.Filter() || this.Filter().length == 0) {
+                    return true;
+                }
+                var localInquiry = this.Inquiry;
+                for (var property in localInquiry) {
+                    if (localInquiry.hasOwnProperty(property) &&
+                        String(localInquiry[property]).contains(this.Filter())) {
+                        return true;
+                    }
+                }
+                return false;
+            }, {
+                Filter: window.viewModel.Filter,
+                Inquiry: inquiry
+            });
+        }
+    }
+
+    ko.applyBindings(window.viewModel);
+});
