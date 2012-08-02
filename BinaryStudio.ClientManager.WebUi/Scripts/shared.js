0ef6823fceb74df43015533f91bd20a055320b55
Ivan Zaporozhchenko <1van1111@mail.ru>
2012-08-02 16:04:43 +0300

    Added cut extension to string type in javascript. Added SkypeLink and EmailLink to inquiry in javascript's viewModel. closed #108

--- BinaryStudio.ClientManager.WebUi/Scripts/shared.js
+++ BinaryStudio.ClientManager.WebUi/Scripts/shared.js
@@ -1,5 +1,8 @@
 ﻿//
 // commonly used functions and methods
+
+//
+//return true if string ends with argument
 String.prototype.endsWith = function (suffix) {
     return this.indexOf(suffix, this.length - suffix.length) !== -1;
 };
@@ -9,6 +12,16 @@ String.prototype.endsWith = function (suffix) {
 // return true if string contains argument
 String.prototype.contains = function(searchString) {
     return this.indexOf(searchString) != -1;
+};
+
+//
+//
+/// If string is more then maximum length then it will be cutten for maximum length and last 3 symbols will be changed for "..."
+/// For example:
+/// var s="This is string";
+/// var result=s.Cut(7); //result will be equal "This..."
+String.prototype.cut = function (maxLength) {
+     return this.length > maxLength ? this.substring(0, maxLength - 3) + "..." : this;
 };
 
 
