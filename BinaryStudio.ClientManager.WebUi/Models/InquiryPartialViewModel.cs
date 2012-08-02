47f5283cdc54c63243ce0ee7ef91a3272e2b3c14
Ivan Zaporozhchenko <1van1111@mail.ru>
2012-08-02 10:35:18 +0300

    Added DetailsUri for InquiryPartialViewModel.cs issue #103

--- BinaryStudio.ClientManager.WebUi/Models/InquiryPartialViewModel.cs
+++ BinaryStudio.ClientManager.WebUi/Models/InquiryPartialViewModel.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections.Generic;
+using System.Web;
+using System.Web.Mvc;
 using BinaryStudio.ClientManager.DomainModel.Entities;
 
 namespace BinaryStudio.ClientManager.WebUi.Models
@@ -10,5 +12,17 @@ namespace BinaryStudio.ClientManager.WebUi.Models
         public IList<Person> Employees { get; set; }
 
         public IList<Tag> Tags { get; set; }
+
+        /// <summary>
+        /// Uri for details of current inquiry
+        /// </summary>
+        public string DetailsUri
+        {
+            get
+            {
+                var url = new UrlHelper(HttpContext.Current.Request.RequestContext);
+                return url.Action("Details", "Inquiries", new { id = Inquiry.Id });
+            }
+        }
     }
 }
