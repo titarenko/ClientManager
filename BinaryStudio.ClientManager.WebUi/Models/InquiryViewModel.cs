a9eb7ae68ff174fbc3265fd0f187dbb5f6cfd9de
Ivan Zaporozhchenko <1van1111@mail.ru>
2012-08-02 11:44:52 +0300

    Added DetailsUri to InquiryViewModel. issue #101

--- BinaryStudio.ClientManager.WebUi/Models/InquiryViewModel.cs
+++ BinaryStudio.ClientManager.WebUi/Models/InquiryViewModel.cs
@@ -1,3 +1,8 @@
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using BinaryStudio.ClientManager.DomainModel.Entities;
+
 namespace BinaryStudio.ClientManager.WebUi.Models
 {
     public class InquiryViewModel
@@ -15,5 +20,17 @@ namespace BinaryStudio.ClientManager.WebUi.Models
         public string PhotoUri { get; set; }
 
         public string Assignee { get; set; }
+
+        /// <summary>
+        /// Uri for details of current inquiry
+        /// </summary>
+        public string DetailsUri
+        {
+            get
+            {
+                var url = new UrlHelper(HttpContext.Current.Request.RequestContext);
+                return url.Action("Details", "Inquiries",new {id = Id});
+            }
+        }
     }
 }
