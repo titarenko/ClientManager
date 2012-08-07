c7796ab20ebeb999188ae480193cfcd3004b2600
Ivan Zaporozhchenko <1van1111@mail.ru>
2012-08-07 14:52:22 +0300

    Extracted MonthViewFiltering.js from Month.cshtml. fixed #114 Extracted AllViewFiltering.js from All.cshtml

---
+++ BinaryStudio.ClientManager.WebUi/Scripts/AllViewFiltering.js
@@ -0,0 +1,29 @@
+﻿$(function() {
+    window.viewModel.Filter = ko.observable('');
+    for (var categoryIndex in window.viewModel.Categories) {
+        var category = window.viewModel.Categories[categoryIndex];
+        for (var inquiryIndex in category.Inquiries) {
+            var inquiry = category.Inquiries[inquiryIndex];
+            inquiry.Tag = category.Tag.Name;
+            inquiry.InquiryDetailsUrl = window.viewModel.InquiryDetailsUrl + '/' + inquiry.Id;
+            inquiry.Visible = ko.computed(function() {
+                if (this.Filter().length == 0) {
+                    return true;
+                }
+                var localInquiry = this.Inquiry;
+                for (var property in localInquiry) {
+                    if (localInquiry.hasOwnProperty(property) &&
+                        property != 'InquiryDetailsUrl' &&
+                            String(localInquiry[property]).contains(this.Filter())) {
+                        return true;
+                    }
+                }
+                return false;
+            }, {
+                Filter: window.viewModel.Filter,
+                Inquiry: inquiry
+            });
+        }
+    }
+    ko.applyBindings(window.viewModel);
+});
---
+++ BinaryStudio.ClientManager.WebUi/Scripts/MonthViewFiltering.js
@@ -0,0 +1,87 @@
+﻿$(function () {
+    window.viewModel.Filter = ko.observable();
+
+    window.viewModel.Filter.subscribe(function(newValue) {
+        for (var weekIndex in this.Weeks) {
+            var week = window.viewModel.Weeks[weekIndex];
+            for (var dayIndex in week.Days) {
+                var day = week.Days[dayIndex];
+                var buttonSelector = '#button-' + day.DateString;
+                if ($(buttonSelector).val() == 'Less') {
+                    toggleList(day,0);
+                }
+            }
+        }
+    }, window.viewModel);
+        
+    for (var weekIndex in window.viewModel.Weeks) {
+        var week = window.viewModel.Weeks[weekIndex];
+        for (var dayIndex in week.Days) {
+            var day = week.Days[dayIndex];
+                               
+            for (var inquiryIndex in day.Inquiries) {
+                var inquiry = day.Inquiries[inquiryIndex];
+                
+                inquiry.day = new Date(day.DateString).getDate();
+                                                                                
+                inquiry.Visible = ko.computed(function() {
+                    if (!this.Filter() || this.Filter().length == 0 ) {
+                        return true;
+                    }
+                    var localInquiry = this.Inquiry;
+                    for (var property in localInquiry) {
+                        if (localInquiry.hasOwnProperty(property) &&
+                            String(localInquiry[property]).contains(this.Filter())) {
+                            return true;
+                        }
+                    }
+                    return false;
+                }, {
+                    Filter: window.viewModel.Filter,
+                    Inquiry: inquiry,
+                    Day: day
+                });
+                    
+                inquiry.isHidden = ko.computed(function() {
+                    var inquiryCurrentVisibleIndex = -1;
+                    for (var inquiryIndex in this.Day.Inquiries) {
+                        var inquiry = this.Day.Inquiries[inquiryIndex];
+                        if (inquiry.Visible()) {
+                            inquiryCurrentVisibleIndex++;
+                            if (this.Index==inquiryIndex)
+                                break;
+                        }
+                    }
+                    return inquiryCurrentVisibleIndex > this.ViewModel.MaxInquiriesWithoutToggling ? true : false;
+                },
+                {
+                    ViewModel: window.viewModel,
+                    Day: day,
+                    Index: inquiryIndex
+                });
+            }
+                    
+                
+            day.visibleInquiriesCount = ko.computed(function() {
+                var inquiryCurrentVisibleIndex = -1;
+                for (var inquiryIndex in this.Inquiries) {
+                    var inquiry = this.Inquiries[inquiryIndex];
+                    if (inquiry.Visible()) {
+                        inquiryCurrentVisibleIndex++;
+                    }
+                }
+                return inquiryCurrentVisibleIndex;
+            },day);
+                
+
+
+            day.isToggle = ko.computed(function() {
+                if (this.visibleInquiriesCount()>window.viewModel.MaxInquiriesWithoutToggling+1) {
+                    return true;
+                }
+                return false;
+            },day);
+        }
+    }
+    ko.applyBindings(window.viewModel);
+});    
