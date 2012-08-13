a2c0c4041d75c85419361eff665f294644fe3d14
yakimechko <yakimechko@gmail.com>
2012-08-13 22:39:37 +0300

    issue #130 added Team entity

---
+++ BinaryStudio.ClientManager.DomainModel/Entities/Team.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using BinaryStudio.ClientManager.DomainModel.Infrastructure;
+
+namespace BinaryStudio.ClientManager.DomainModel.Entities
+{
+    /// <summary>
+    /// Represents team of users
+    /// </summary>
+    class Team: IIdentifiable
+    {
+        /// <summary>
+        /// Unique identifier
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Name of the team
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// List of users who belong to the team
+        /// </summary>
+        public List<User> Users { get; set; }
+    }
+}
