11d655fe14ac97bf6fa31714add1708ca9e8d317
yakimechko <yakimechko@gmail.com>
2012-08-14 17:08:33 +0300

    created Teams controller

---
+++ BinaryStudio.ClientManager.WebUi/Controllers/TeamsController.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Mvc;
+using BinaryStudio.ClientManager.DomainModel.DataAccess;
+
+namespace BinaryStudio.ClientManager.WebUi.Controllers
+{
+    [Authorize]
+    public class TeamsController : Controller
+    {
+        private IRepository repository;
+
+        public TeamsController(IRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        public ViewResult Index()
+        {
+            return View();
+        }
+    }
+}
