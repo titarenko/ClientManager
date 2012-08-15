96aada26af9c058393cfee0082eb97da42c4e2b1
yakimechko <yakimechko@gmail.com>
2012-08-15 16:40:20 +0300

    deleted unused UserService

--- BinaryStudio.ClientManager.DomainModel/Infrastructure/UserService.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace BinaryStudio.ClientManager.DomainModel.Infrastructure
-{
-    public class UserService:IIdentifiable
-    {
-        public int Id { get; set; }
-        public IIdentifiable CurrentUser { get; set; }
-    }
-}
