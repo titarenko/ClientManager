526b2f6579330e75898ab1a521d7591ef0872499
yakimechko <yakimechko@gmail.com>
2012-08-16 20:34:21 +0300

    issue #134 added EmployeeViewModel and TeamsViewModel classes

---
+++ BinaryStudio.ClientManager.WebUi/Models/EmployeeViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace BinaryStudio.ClientManager.WebUi.Models
+{
+    public class EmployeeViewModel
+    {
+        public string label { get; set; }
+
+        public int value { get; set; }
+    }
+}
