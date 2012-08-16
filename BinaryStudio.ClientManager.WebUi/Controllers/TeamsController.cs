30c13c428714c47ba9c25ea660c08f0a2df3ac76
yakimechko <yakimechko@gmail.com>
2012-08-16 10:18:09 +0300

    added IAppContext as a dependency into Teams controller constructor

--- BinaryStudio.ClientManager.WebUi/Controllers/TeamsController.cs
+++ BinaryStudio.ClientManager.WebUi/Controllers/TeamsController.cs
@@ -1,16 +1,20 @@
 ﻿using System.Web.Mvc;
 using BinaryStudio.ClientManager.DomainModel.DataAccess;
+using BinaryStudio.ClientManager.DomainModel.Infrastructure;
 
 namespace BinaryStudio.ClientManager.WebUi.Controllers
 {
     [Authorize]
     public class TeamsController : Controller
     {
-        private IRepository repository;
+        private readonly IRepository repository;
 
-        public TeamsController(IRepository repository)
+        private readonly IAppContext appContext;
+
+        public TeamsController(IRepository repository, IAppContext appContext)
         {
             this.repository = repository;
+            this.appContext = appContext;
         }
 
         public ViewResult Index()
